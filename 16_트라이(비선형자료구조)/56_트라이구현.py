b5eb86ea3bc427fbd7ee4a3f5c71f9b1c792f66f
sangyeob Kim <rlatkdduq99@medicalai.com>
2024-09-16 19:39:02 +0900

    56.팰린드롬페어.py

---
+++ 16_트라이(비선형자료구조)/56_트라이구현.py
@@ -0,0 +1,54 @@
+# 0. dict로 구현, 시간복잡도/공간복잡도 신경 안씀
+import collections 
+
+class Trie:
+    
+    def __init__(self):
+        self.trie = None
+        self.trie = collections.defaultdict(list)
+
+    def insert(self, word: str) -> None:
+        for n in range(len(word)) : 
+            self.trie[word[:n+1]].append(word)
+
+    def search(self, word: str) -> bool:
+        if word in self.trie and word in self.trie[word] : 
+            return True
+        return False
+        
+    def startsWith(self, prefix: str) -> bool:
+        if prefix in self.trie :
+            return True
+        
+# 1. 트라이로 개선된 형태, 트라이를 통해 dict에서의 불필요한 key mapping은 최소화할 수 있음.
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word : 
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word : 
+            if char not in node.children : 
+                return False
+            node = node.children[char]
+        return node.word
+        
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix : 
+            if char not in node.children : 
+                return False
+            node = node.children[char]
+        return True
+        
